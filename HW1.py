52f227fcb8c17ac1a5b18aaaa835a5d8115df045
Natalia Zarubkina <tasha3425@mail.ru>
2024-07-23 18:11:11 +0300

    Add HW1

---
+++ HW1.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+# Ваши учетные данные API
+client_id = "__"
+client_secret = "__"
+
+# Конечная точка API
+endpoint = "https://api.foursquare.com/v3/places/search"
+
+# Определение параметров для запроса API
+city = input("Введите название города: ")
+category = input("Введите категорию заведения: ")
+params = {
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "near": city,
+    "query": category
+}   
+
+headers = {
+    "Accept": "application/json",
+    "Authorization": "fsq3V3AFHzvqod5PVkb9j5ptfec29VfLTGG2XbHrQEGC8bI="
+}
+
+response = requests.get(endpoint, params=params, headers=headers)
+
+# Проверка успешности запроса API
+if response.status_code == 200:
+    print("Успешный запрос API!")
+    data = json.loads(response.text) # Парсим JSON-ответ в словарь Python
+    venues = data["results"] # Получаем список мест из данных ответа
+    for venue in venues:# Проходимся по каждому месту в списке
+        print("Название:", venue["name"])
+        try:
+            print("Адрес:", venue["location"]["address"])
+        except Exception:
+            print("Адрес не найден:")
+        print("\n")
+else:
+    print("Запрос API завершился неудачей с кодом состояния:", response.status_code)
+    print(response.text)
